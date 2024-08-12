e9178a8a101656b74b7f3762d0cc664a304628d1
MJ Polok <mjpolok018@gmail.com>
2024-08-13 01:21:16 +0600

    Connect to DB

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 import express from "express"
 import dotenv from "dotenv"
+import mongoose from "mongoose"
 
 dotenv.config()
 
-const app = express() 
+
+const dburi = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ll8pd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const app = express()
 const PORT = process.env.PORT || 3000
+
+mongoose.connect(dburi)
+    .then(() => {
+        console.log("connected to mongoDB");
+    })
+    .catch(err => {
+        console.log(err);
+    })
 
 app.get("/", (req, res) => {
     res.send("Hello Every One")
 })
 app.listen(3000, () => {
-    console.log(`Server is running on port ${PORT}`);
-    
+    console.log(`Server is running on port ${PORT}`);    
 })
